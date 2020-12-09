77d933d712c0756d3ffafb14f2c1d59e3f4745b0
Pawan Benjamin <pawanbenjamin@gmail.com>
2020-12-09 10:49:59 -0500

    webpack initialized

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "client", "index"),
+  output: {
+    path: path.resolve(__dirname, "src", "public"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
