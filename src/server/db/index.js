ef6057465d517342274ea95b79e683ea51776d14
Pawan Benjamin <pawanbenjamin@gmail.com>
2021-01-16 22:18:59 -0500

    postgres from heroku

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -5,8 +5,11 @@ const dbName = pkg.name;
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+  "postgres",
+  "",
   {
     logging: false,
+    dialect: "postgres",
   }
 );
 
