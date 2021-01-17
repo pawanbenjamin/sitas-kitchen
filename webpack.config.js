a5ef05ee7aadb67c5f40b28786bbe540bc1bb77f
Pawan Benjamin <pawanbenjamin@gmail.com>
2021-01-16 22:03:53 -0500

    more debugging

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, "src", "public"),
     filename: "bundle.js",
   },
-  mode: "development",
+  mode: "production",
   module: {
     rules: [
       {
