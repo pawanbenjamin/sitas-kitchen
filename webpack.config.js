2b49f799d66d8fe9e4d7d662d102a33662f3d562
Pawan Benjamin <pawanbenjamin@gmail.com>
2021-01-16 23:47:54 -0500

    revert to localhost

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, "src", "public"),
     filename: "bundle.js",
   },
-  mode: "production",
+  mode: "development",
   module: {
     rules: [
       {
